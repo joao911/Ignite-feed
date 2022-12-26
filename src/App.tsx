9dd5f2f03d461aca9d32a995b61109f4101d4a97
João Paulo Duarte <joao.sousa@tangotech.com.br>
2022-12-26 11:08:56 -0300

    feat: project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,45 @@
 import { Header, Post, Sidebar } from "./components";
-import { map } from 'lodash'
+import { map } from "lodash";
 import "./global.css";
 
-
-
 function App() {
-
   const posts = [
     {
       id: 1,
       author: {
         avatarUrl: "https://github.com/joao911.png",
-        name: 'João Paulo',
-        role: 'Desenvolvedor frot end'
+        name: "João Paulo",
+        role: "Desenvolvedor frot end",
       },
-      content: [{ type: 'paragraph', content: 'Fala galera' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto pro meu portifólio' },
-      { type: 'link', content: 'joao911/fundamentos' }
+      content: [
+        { type: "paragraph", content: "Fala galera" },
+        {
+          type: "paragraph",
+          content: "Acabei de subir mais um projeto pro meu portifólio",
+        },
+        { type: "link", content: "joao911/fundamentos" },
       ],
-      publishAta: new Date('2022-05-03 20:00:00')
+      publishAta: new Date("2022-05-03 20:00:00"),
     },
     {
       id: 2,
       author: {
         avatarUrl: "https://github.com/joao911.png",
-        name: 'João Paulo',
-        role: 'Desenvolvedor frot end'
+        name: "João Paulo",
+        role: "Desenvolvedor frot end",
       },
-      content: [{ type: 'paragraph', content: 'Fala galera' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa, É um projeto que fiz pra aprimorar meus conhecimentos em tailwind css' },
-      { type: 'link', content: 'joao911/fundamentos' }
+      content: [
+        { type: "paragraph", content: "Fala galera" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa, É um projeto que fiz pra aprimorar meus conhecimentos em tailwind css",
+        },
+        { type: "link", content: "joao911/fundamentos" },
       ],
-      publishAta: new Date('2022-10-03 20:00:00')
-    }
-  ]
+      publishAta: new Date("2022-10-03 20:00:00"),
+    },
+  ];
 
   return (
     <div>
@@ -42,13 +48,10 @@ function App() {
         <Sidebar />
 
         <div className="flex flex-col gap-9 w-full">
-          {
-            map(posts, item => (
-              <Post item={item} key={item.id}/>
-            ))
-          }
+          {map(posts, (item) => (
+            <Post item={item} key={item.id} />
+          ))}
         </div>
-
       </div>
     </div>
   );
